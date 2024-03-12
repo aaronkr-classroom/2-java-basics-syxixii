8aec1465ec1b06b27a70fe896f3e06f7040955e9
syxixii <sys47201@gmail.com>
2024-03-12 17:10:37 +0900

    20211829 송영후-Example04,05

---
+++ Example05.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Example02 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Type your name: ");
+        String name = s.nextLine();
+        System.out.println("Welcome " + name + "!");
+
+        System.out.println("Type your age: ");
+        int age = s.nextInt();
+        System.out.println("Your age: " + age);
+        
+    }
+
+}
